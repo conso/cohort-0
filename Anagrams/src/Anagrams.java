4bd028bf0b01cc7cb4a2b76cd352c2868cc6cc07
Marco Consolaro <marco@alcortech.net>
2020-07-27 12:38:07 +0100

    cleaned up

--- Anagrams/src/Anagrams.java
+++ Anagrams/src/Anagrams.java
@@ -2,40 +2,24 @@ import java.util.ArrayList;
 
 public class Anagrams {
     public String solve(String input) {
-        ArrayList<String> output = new ArrayList<String>();
-
+        ArrayList<String> output;
         output = calculateAnagramsFor(input);
-
         return String.join(" ", output);
     }
 
     private ArrayList<String> calculateAnagramsFor(String input){
-        ArrayList<String> output = new ArrayList<String>();
-        if (input.length()==1) output.add(input);
+        ArrayList<String> output = new ArrayList<>();
+        if (input.length()==1) output.add(input); //exit condition for recursion
+
         for(int i = 0; i < input.length(); i++) {
             String character = String.valueOf(input.charAt(i));
             String inputLeftover = this.removeCharAt(input, i);
-
             ArrayList<String> partialAnagrams = this.calculateAnagramsFor(inputLeftover);
-            for (int x=0; x<partialAnagrams.size(); x++) {
-                output.add(character + partialAnagrams.get(x));
+
+            for (String partialAnagram : partialAnagrams) {
+                output.add(character + partialAnagram);
             }
         }
-        return output;
-    }
-
-    private ArrayList<String> anagramOfAny(String inputLeftover) {
-        ArrayList<String> output = new ArrayList<String>();
-        for (int l = 0; l<inputLeftover.length(); l++){
-            output.add(String.valueOf(inputLeftover.charAt(l)));
-        }
-        return output;
-    }
-
-    private ArrayList<String> anagramOfTwo(String input) {
-        ArrayList<String> output = new ArrayList<String>();
-        output.add(input);
-        output.add(this.reverse(input));
         return output;
     }
 
@@ -45,11 +29,4 @@ public class Anagrams {
         return sb.toString();
     }
 
-    private String reverse(String input) {
-        String reverse = "";
-        for(int i = input.length() - 1; i >= 0; i--) {
-            reverse = reverse + input.charAt(i);
-        }
-        return reverse;
-    }
 }
